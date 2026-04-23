92cdc96e153c952fa6c579eda89ac51d56c998c3
aaheikki <aaheikki@uef.fi>
2026-04-23 09:36:07 +0300

    Interrupt of clock working -> add screen

--- lowP_clock_source/lowP_clock_source.ino
+++ lowP_clock_source/lowP_clock_source.ino
@@ -1,14 +1,16 @@
 #include <RTClib.h>
 
-
 RTC_DS3231 rtc; // define rtc object
 
 #define CLOCK_INTERRUPT_PIN 2
+#define ALARM_INTERVAL 5
 
-volatile bool CLOCK_INT_ALARM = false;
+volatile bool clock_flag = false;
+DateTime time_now;
 
-void onClock_Interrupt(void) { //Interrupt to change flag -> main work in loop()
-    clock_flag = true; 
+
+void onClock_Interrupt(void) { //Interrupt to set tiem and to change flag -> main work in loop()
+  clock_flag = true; 
 }
 
 void setup() {
@@ -19,33 +21,34 @@ void setup() {
     Serial.println("Could not find RTC!");
   }
   if(rtc.lostPower()) {
-    Serial.println("RTC lost power, set time!")
+    Serial.println("RTC lost power, set time!");
   }
   rtc.disable32K();                         // Disable 32K pin
   rtc.clearAlarm(1);rtc.clearAlarm(2);      // Clear and disable alarms 1 and 2, since they are not reset with MCU board
   rtc.disableAlarm(1); rtc.disableAlarm(2);
   rtc.writeSqwPinMode(DS3231_OFF);          // Stop INT/SQW pin
   
-  if(!rtc.setAlarm1(rtc.now() + TimeSpan(10),DS3231_A1_Second)) { // schedule an alarm 10 seconds in the future
+  if(!rtc.setAlarm1(rtc.now() + TimeSpan(ALARM_INTERVAL),DS3231_A1_Second)) { // schedule an alarm ALARM_INTERVAL seconds in the future
       Serial.println("Error, alarm wasn't set!");
   }
-}
-
 
   //Interrut pins
-  pinMode(CLOCK_INTERRUPT_PIN, INPUT_PULLUP)
+  pinMode(CLOCK_INTERRUPT_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN), onClock_Interrupt, FALLING);
 }
 
 void loop() {
-  char time[10] = "hh:mm:ss"; // Initialize time char
   
-  // print current time if x time has passed
+  // Handel clock interrupt
   if(clock_flag){  
     clock_flag = false;
-    rtc.now().toString(time);
-    Serial.print(time);  
-  }
-  
+    time_now = rtc.now();
+    char time_char[10] = "hh:mm:ss"; // Initialize time char
+    time_now.toString(time_char);
+    //rtc.now().toString(time_char);
+    Serial.println(time_char);
+    rtc.clearAlarm(1);
+    rtc.setAlarm1(time_now + TimeSpan(ALARM_INTERVAL),DS3231_A1_Second); //set alarm ALARM_INTERVAL seconds to the future
+  }  
 
 }
